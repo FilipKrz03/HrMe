1324e2f51ebab901fbccb00ef9090407ce6f0a95
Filip Krzemień <filipos2003@gmail.com>
2023-11-05 13:02:30 +0100

    Added validation to WageQueries

---
+++ Application/CQRS/WageQueries/GetWageForMonth/GetWageForMonthQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.WageQueries.GetWageForMonth
+{
+    public class GetWageForMonthQueryValidator : AbstractValidator<GetWageForMonthQuery>
+    {
+        public GetWageForMonthQueryValidator()
+        {
+            RuleFor(x => x.Year).GreaterThan(2000).LessThan(2050).WithMessage("Enter valid year");
+            RuleFor(x => x.Month).GreaterThan(0).LessThan(13).WithMessage("Enter valid month");
+        }
+    }
+}
